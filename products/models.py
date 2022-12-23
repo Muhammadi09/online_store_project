d6959ed118b1e0682d5567ca2d9d988aa429ced5
muhammad <muhammad.i09@protonmail.com>
2022-12-23 18:29:29 +0000

    product and categy v2

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
 
     CATEGORY_CHOICES = (
-        ('books', 'Books'), 
+        ('film', 'Film'), 
         ('home', 'Home'),
         ('clothes', 'Clothes'),
         ('health', 'Health'),
@@ -25,8 +25,9 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    cover = models.ImageField(upload_to='covers/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=6)
+    cover = models.ImageField(upload_to='covers/')
+    
 
     def __str__(self):
         return self.title
